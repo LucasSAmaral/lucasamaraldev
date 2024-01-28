65654591c1af5c500ad26f2ba962638492ff0026
LucasSAmaral <lucassantosamaral@hotmail.com>
2024-01-27 21:42:52 -0300

    feat: cria about me

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,24 @@
+import { AboutMeWrapper } from "../styles/styles";
+
+const AboutMe = () => {
+  return (
+    <AboutMeWrapper>
+      <h2>Sobre Mim</h2>
+      <p>
+        Meu nome é Lucas Amaral. Tenho 31 anos. Moro no Rio de Janeiro e sou
+        desenvolvedor front-end.
+      </p>
+      <p>
+        Gosto de conversar sobre a cultura pop, mas especificamente sobre
+        cinema. Gosto de assistir filmes e séries, jogar video game e ler livros
+        e quadrinhos no meu tempo livre.
+      </p>
+      <p>
+        Me formei em Sistemas de Informação em 2014 e trabalho como
+        desenvolvedor front-end desde 2015.
+      </p>
+    </AboutMeWrapper>
+  );
+};
+
+export default AboutMe;
--- src/app/about/page.tsx
+++
@@ -1,5 +0,0 @@
-const About = () => {
-  return <h1>Sobre</h1>;
-};
-
-export default About;
