105ac53dbf9077d2bbcdea02cd3382f0a766956c
Lucas Amaral <lucas.samaral@b2wdigital.com>
2024-04-18 18:06:38 -0300

    wip: começo do desenvolvimento do form de email

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,19 +1,25 @@
+'use client';
 import React from 'react';
 import { MenuProvider } from './components/menu/Menu.context';
 import { ModalProvider } from './components/modal/Modal.context';
 import GlobalStyle from './styles/globalStyle';
 import { LanguageOptionsProvider } from './components/language-options/LanguageOptions.context';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <LanguageOptionsProvider>
-      <ModalProvider>
-        <MenuProvider>
-          <GlobalStyle />
-          {children}
-        </MenuProvider>
-      </ModalProvider>
-    </LanguageOptionsProvider>
+    <QueryClientProvider client={queryClient}>
+      <LanguageOptionsProvider>
+        <ModalProvider>
+          <MenuProvider>
+            <GlobalStyle />
+            {children}
+          </MenuProvider>
+        </ModalProvider>
+      </LanguageOptionsProvider>
+    </QueryClientProvider>
   );
 };
 
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,15 +1,79 @@
-import type { Metadata } from 'next';
+'use client';
+// import type { Metadata } from 'next';
+import { useMutation } from '@tanstack/react-query';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import axios from 'axios';
 
-export const metadata: Metadata = {
-  title: 'Contato',
-  description: 'Entre em contato comigo',
+type FormValues = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
 };
 
-const Contact = () => {
+// export const metadata: Metadata = {
+//   title: 'Contato',
+//   description: 'Entre em contato comigo',
+// };
+
+const Contact: React.FC = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  console.log('errors', errors);
+
+  const { mutate: sendEmail } = useMutation({
+    mutationFn: async (formValues: FormValues) => {
+      await axios.post('https://api.emailjs.com/api/v1.0/email/send-form', formValues);
+    },
+  });
+
+  const onSubmit = handleSubmit(formValues => {
+    // sendEmail(formValues);
+    console.log('valores', formValues);
+  });
   return (
-    <div>
-      <h2>Contato</h2>
-    </div>
+    <form onSubmit={onSubmit}>
+      <Controller
+        name="name"
+        control={control}
+        rules={{ required: 'Obrigatorio' }}
+        render={({ field: { name, onChange, onBlur, value } }) => (
+          <>
+            <input type="text" name={name} onBlur={onBlur} onChange={onChange} value={value} />
+            {errors[name] && <span>{errors[name]?.message}</span>}
+          </>
+        )}
+      />
+      <Controller
+        name="email"
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { name, onChange, onBlur, value } }) => (
+          <input type="text" name={name} onBlur={onBlur} onChange={onChange} value={value} />
+        )}
+      />
+      <Controller
+        name="subject"
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { name, onChange, onBlur, value } }) => (
+          <input type="text" name={name} onBlur={onBlur} onChange={onChange} value={value} />
+        )}
+      />
+      <Controller
+        name="message"
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { name, onChange, onBlur, value } }) => (
+          <input type="text" name={name} onBlur={onBlur} onChange={onChange} value={value} />
+        )}
+      />
+      <button type="submit">CRICA</button>
+    </form>
   );
 };
 
