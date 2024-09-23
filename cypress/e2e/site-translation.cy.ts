779d7b8a6096f0068db18c0dcb72aad13a95fb3b
Lucas Amaral <lucas.samaral@b2wdigital.com>
2024-09-23 09:33:55 -0300

    test: cria teste para verificar a tradução após carregar a página com o queryParam lang

--- cypress/e2e/site-translation.cy.ts
+++ cypress/e2e/site-translation.cy.ts
@@ -3,7 +3,7 @@ import CONFIG from '../../generated-config.json';
 const { locale } = CONFIG;
 
 describe('Site translation', () => {
-  it('should check if all site is translated', () => {
+  it('should check if all site is translated after clicking on language-button', () => {
     cy.visit('/');
 
     cy.getDataCy('language-button').click();
@@ -25,6 +25,25 @@ describe('Site translation', () => {
     // cy.wait(1000).then(() => {
     //   cy.get('.close-modal').click();
     // });
+
+    cy.getDataCy('contact-option').click();
+    cy.get('body').contains(locale['en'].contact.form.labels.fromName);
+    cy.get('body').contains(locale['en'].contact.form.labels.replyTo);
+    cy.get('body').contains(locale['en'].contact.form.labels.subject);
+    cy.get('body').contains(locale['en'].contact.form.labels.message);
+    cy.get('body').contains(locale['en'].contact.form.sendButtonText);
+  });
+
+  it('should check if all site is translated after loading with lang queryParam', () => {
+    cy.visit('/?lang=en');
+
+    cy.getDataCy('subtitle').contains(locale['en'].home.profission);
+
+    cy.getDataCy('about-me-option').click();
+    cy.get('body').contains(locale['en'].aboutMe.paragraph1);
+
+    cy.getDataCy('work-experience-option').click();
+    cy.get('body').contains(locale['en'].workExperience.company1.periodWorked);
 
     cy.getDataCy('contact-option').click();
     cy.get('body').contains(locale['en'].contact.form.labels.fromName);
